0a97e43a0c949575de79d826dc0026978f954e4f
atri-konami <freude1994@gmail.com>
2021-10-09 17:10:22 +0900

    change directory structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const common = require("./webpack.config.common");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   ...common,
@@ -9,6 +10,18 @@ module.exports = {
     chunkFilename: "js/chunks/[name]-chunk-[chunkhash].js",
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ["macro_copy"],
+      filename: "macro_copy/index.html",
+      template: "./src/template/macro_copy/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ["timeapp"],
+      filename: "timeapp/index.html",
+      template: "./src/template/timeapp/index.html",
+    }),
+  ],
   devtool: "inline-source-map",
   devServer: {
     //contentBase: __dirname + '/public'
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
 const common = require("./webpack.config.common");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   ...common,
@@ -9,4 +10,16 @@ module.exports = {
     filename: "js/[name]-[fullhash].js",
     chunkFilename: "js/chunks/[name]-chunk-[chunkhash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ["macro_copy"],
+      filename: "product/macro_copy/index.html",
+      template: "./src/template/macro_copy/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ["timeapp"],
+      filename: "product/timeapp/index.html",
+      template: "./src/template/timeapp/index.html",
+    }),
+  ],
 };
